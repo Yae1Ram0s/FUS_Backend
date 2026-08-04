6f0370070b1b6b32206cb858a6220a6923f0f0fc
Yae1Ram0s <126137429+Yae1Ram0s@users.noreply.github.com>
2026-08-04 14:22:53 -0600

    Optimiza envio de correos OTP

--- backend/settings.py
+++ backend/settings.py
@@ -226,3 +226,4 @@ EMAIL_USE_TLS      = os.environ.get('EMAIL_USE_TLS', 'True').lower() in ('true',
 EMAIL_HOST_USER    = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD= os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'noreply@anam.gob.mx')
+EMAIL_TIMEOUT      = int(os.environ.get('EMAIL_TIMEOUT', 10))
